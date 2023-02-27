8f331833019547d20a69977bc83dbf7f31beabac
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-02-26 22:55:56 -0400

    [UPDATE] colorFormatConverter arguments and return

--- src/convert/colorFormatConverter.ts
+++ src/convert/colorFormatConverter.ts
@@ -4,175 +4,192 @@ import converter from './index'
 type CurrentValue = Cmyk | string | Hsl | Hsv | Rgb
 type FinalValue = Cmyk | string | Hsl | Hsv | Lab | Rgb | Xyz
 
-function colorFormatConverter(
-  currentFormat: string, 
-  value: CurrentValue, 
-  targetFormat: string
-): FinalValue {
-  let finalValue: FinalValue
+interface Options {
+  currentFormat: string
+  targetFormat?: Array<string>
+  AllFormats?: boolean
+}
 
-  // CMYK to - convertions
-  if (
-    currentFormat === 'cmyk' &&
-    targetFormat ===  'hex'
-  ) {
-    finalValue = converter.cmykToHex(value as Cmyk)
-  } else if (
-    currentFormat === 'cmyk' &&
-    targetFormat === 'hsl'
-  ) {
-    finalValue = converter.cmykToHsl(value as Cmyk)
-  } else if (
-    currentFormat === 'cmyk' &&
-    targetFormat === 'hsv'
-  ) {
-    finalValue = converter.cmykToHsv(value as Cmyk)
-  } else if (
-    currentFormat === 'cmyk' &&
-    targetFormat === 'lab'
-  ) {
-    finalValue = converter.cmykToLab(value as Cmyk)
-  } else if (
-    currentFormat === 'cmyk' &&
-    targetFormat === 'rgb'
-  ) {
-    finalValue = converter.cmykToRgb(value as Cmyk)
-  } else if (
-    currentFormat === 'cmyk' &&
-    targetFormat === 'xyz'
-  ) {
-    finalValue = converter.cmykToXyz(value as Cmyk)
-  }
-  // Hexadecimal to - convertions
-  else if (
-    currentFormat === 'hex' &&
-    targetFormat === 'cmyk'
-  ) {
-    finalValue = converter.hexToCmyk(value as string)
-  } else if (
-    currentFormat === 'hex' &&
-    targetFormat === 'hsl'
-  ) {
-    finalValue = converter.hexToHsl(value as string)
-  } else if (
-    currentFormat === 'hex' &&
-    targetFormat === 'hsv'
-  ) {
-    finalValue = converter.hexToHsv(value as string)
-  } else if (
-    currentFormat === 'hex' &&
-    targetFormat === 'lab'
-  ) {
-    finalValue = converter.hexToLab(value as string)
-  } else if (
-    currentFormat === 'hex' &&
-    targetFormat === 'rgb'
-  ) {
-    finalValue = converter.hexToRgb(value as string)
-  } else if (
-    currentFormat === 'hex' &&
-    targetFormat === 'xyz'
-  ) {
-    finalValue = converter.hexToXyz(value as string)
-  }
-  // HSL to - convertions
-  else if (
-    currentFormat === 'hsl' &&
-    targetFormat === 'cmyk'
-  ) {
-    finalValue = converter.hslToCmyk(value as Hsl)
-  } else if (
-    currentFormat === 'hsl' &&
-    targetFormat === 'hex'
-  ) {
-    finalValue = converter.hslToHex(value as Hsl)
-  } else if (
-    currentFormat === 'hsl' &&
-    targetFormat === 'hsv'
-  ) {
-    finalValue = converter.hslToHsv(value as Hsl)
-  } else if (
-    currentFormat === 'hsl' &&
-    targetFormat === 'lab'
-  ) {
-    finalValue = converter.hslToLab(value as Hsl)
-  } else if (
-    currentFormat === 'hsl' &&
-    targetFormat === 'rgb'
-  ) {
-    finalValue = converter.hslToRgb(value as Hsl)
-  } else if (
-    currentFormat === 'hsl' &&
-    targetFormat === 'xyz'
-  ) {
-    finalValue = converter.hslToXyz(value as Hsl)
-  }
-  // HSV to - convertions
-  else if (
-    currentFormat === 'hsv' &&
-    targetFormat === 'cmyk'
-  ) {
-    finalValue = converter.hsvToCmyk(value as Hsv)
-  } else if (
-    currentFormat === 'hsv' &&
-    targetFormat === 'hex'
-  ) {
-    finalValue = converter.hsvToHex(value as Hsv)
-  } else if (
-    currentFormat === 'hsv' &&
-    targetFormat === 'hsl'
-  ) {
-    finalValue = converter.hsvToHsl(value as Hsv)
-  } else if (
-    currentFormat === 'hsv' &&
-    targetFormat === 'lab'
-  ) {
-    finalValue = converter.hsvToLab(value as Hsv)
-  } else if (
-    currentFormat === 'hsv' &&
-    targetFormat === 'rgb'
-  ) {
-    finalValue = converter.hsvToRgb(value as Hsv)
-  } else if (
-    currentFormat === 'hsv' &&
-    targetFormat === 'xyz'
-  ) {
-    finalValue = converter.hsvToXyz(value as Hsv)
-  }
-  // RGB to - convertions
-  else if (
-    currentFormat === 'rgb' &&
-    targetFormat === 'cmyk'
-  ) {
-    finalValue = converter.rgbToCmyk(value as Rgb)
-  } else if (
-    currentFormat === 'rgb' &&
-    targetFormat === 'hex'
-  ) {
-    finalValue = converter.rgbToHex(value as Rgb)
-  } else if (
-    currentFormat === 'rgb' &&
-    targetFormat === 'hsl'
-  ) {
-    finalValue = converter.rgbToHsl(value as Rgb)
-  } else if (
-    currentFormat === 'rgb' &&
-    targetFormat === 'hsv'
-  ) {
-    finalValue = converter.rgbToHsv(value as Rgb)
-  } else if (
-    currentFormat === 'rgb' &&
-    targetFormat === 'lab'
-  ) {
-    finalValue = converter.rgbToLab(value as Rgb)
-  } else if (
-    currentFormat === 'rgb' &&
-    targetFormat === 'xyz'
-  ) {
-    finalValue = converter.rgbToXyz(value as Rgb)
-  } else {
-    throw new Error('Format not valid')
-  }
+function colorFormatConverter(
+  // currentFormat: string, 
+  // value: CurrentValue, 
+  // targetFormat: string
+  value: CurrentValue,
+  options: Options
+): Array<FinalValue> {
+  const currentFormat = options.currentFormat
+  const targetFormats = options.AllFormats ? ['cmyk', 'hex', 'hsl', 'hsv', 'lab', 'rgb', 'xyz'] : options.targetFormat
+
+  const finalValue: Array<FinalValue> = []
+
+  targetFormats.map((format) => {
+    // Exclude current format
+    if (format === currentFormat) return
+
+    // CMYK to - convertions
+    if (
+      currentFormat === 'cmyk' &&
+      format ===  'hex'
+    ) {
+      finalValue.push(converter.cmykToHex(value as Cmyk))
+    } else if (
+      currentFormat === 'cmyk' &&
+      format === 'hsl'
+    ) {
+      finalValue.push(converter.cmykToHsl(value as Cmyk))
+    } else if (
+      currentFormat === 'cmyk' &&
+      format === 'hsv'
+    ) {
+      finalValue.push(converter.cmykToHsv(value as Cmyk))
+    } else if (
+      currentFormat === 'cmyk' &&
+      format === 'lab'
+    ) {
+      finalValue.push(converter.cmykToLab(value as Cmyk))
+    } else if (
+      currentFormat === 'cmyk' &&
+      format === 'rgb'
+    ) {
+      finalValue.push(converter.cmykToRgb(value as Cmyk))
+    } else if (
+      currentFormat === 'cmyk' &&
+      format === 'xyz'
+    ) {
+      finalValue.push(converter.cmykToXyz(value as Cmyk))
+    }
+    // Hexadecimal to - convertions
+    else if (
+      currentFormat === 'hex' &&
+      format === 'cmyk'
+    ) {
+      finalValue.push(converter.hexToCmyk(value as string))
+    } else if (
+      currentFormat === 'hex' &&
+      format === 'hsl'
+    ) {
+      finalValue.push(converter.hexToHsl(value as string))
+    } else if (
+      currentFormat === 'hex' &&
+      format === 'hsv'
+    ) {
+      finalValue.push(converter.hexToHsv(value as string))
+    } else if (
+      currentFormat === 'hex' &&
+      format === 'lab'
+    ) {
+      finalValue.push(converter.hexToLab(value as string))
+    } else if (
+      currentFormat === 'hex' &&
+      format === 'rgb'
+    ) {
+      finalValue.push(converter.hexToRgb(value as string))
+    } else if (
+      currentFormat === 'hex' &&
+      format === 'xyz'
+    ) {
+      finalValue.push(converter.hexToXyz(value as string))
+    }
+    // HSL to - convertions
+    else if (
+      currentFormat === 'hsl' &&
+      format === 'cmyk'
+    ) {
+      finalValue.push(converter.hslToCmyk(value as Hsl))
+    } else if (
+      currentFormat === 'hsl' &&
+      format === 'hex'
+    ) {
+      finalValue.push(converter.hslToHex(value as Hsl))
+    } else if (
+      currentFormat === 'hsl' &&
+      format === 'hsv'
+    ) {
+      finalValue.push(converter.hslToHsv(value as Hsl))
+    } else if (
+      currentFormat === 'hsl' &&
+      format === 'lab'
+    ) {
+      finalValue.push(converter.hslToLab(value as Hsl))
+    } else if (
+      currentFormat === 'hsl' &&
+      format === 'rgb'
+    ) {
+      finalValue.push(converter.hslToRgb(value as Hsl))
+    } else if (
+      currentFormat === 'hsl' &&
+      format === 'xyz'
+    ) {
+      finalValue.push(converter.hslToXyz(value as Hsl))
+    }
+    // HSV to - convertions
+    else if (
+      currentFormat === 'hsv' &&
+      format === 'cmyk'
+    ) {
+      finalValue.push(converter.hsvToCmyk(value as Hsv))
+    } else if (
+      currentFormat === 'hsv' &&
+      format === 'hex'
+    ) {
+      finalValue.push(converter.hsvToHex(value as Hsv))
+    } else if (
+      currentFormat === 'hsv' &&
+      format === 'hsl'
+    ) {
+      finalValue.push(converter.hsvToHsl(value as Hsv))
+    } else if (
+      currentFormat === 'hsv' &&
+      format === 'lab'
+    ) {
+      finalValue.push(converter.hsvToLab(value as Hsv))
+    } else if (
+      currentFormat === 'hsv' &&
+      format === 'rgb'
+    ) {
+      finalValue.push(converter.hsvToRgb(value as Hsv))
+    } else if (
+      currentFormat === 'hsv' &&
+      format === 'xyz'
+    ) {
+      finalValue.push(converter.hsvToXyz(value as Hsv))
+    }
+    // RGB to - convertions
+    else if (
+      currentFormat === 'rgb' &&
+      format === 'cmyk'
+    ) {
+      finalValue.push(converter.rgbToCmyk(value as Rgb))
+    } else if (
+      currentFormat === 'rgb' &&
+      format === 'hex'
+    ) {
+      finalValue.push(converter.rgbToHex(value as Rgb))
+    } else if (
+      currentFormat === 'rgb' &&
+      format === 'hsl'
+    ) {
+      finalValue.push(converter.rgbToHsl(value as Rgb))
+    } else if (
+      currentFormat === 'rgb' &&
+      format === 'hsv'
+    ) {
+      finalValue.push(converter.rgbToHsv(value as Rgb))
+    } else if (
+      currentFormat === 'rgb' &&
+      format === 'lab'
+    ) {
+      finalValue.push(converter.rgbToLab(value as Rgb))
+    } else if (
+      currentFormat === 'rgb' &&
+      format === 'xyz'
+    ) {
+      finalValue.push(converter.rgbToXyz(value as Rgb))
+    } else {
+      throw new Error('Format not valid')
+    }
+  })
+
 
   return finalValue
 }
