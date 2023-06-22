9da5ed0be63f7cfef2247da39794e73e94a97e14
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-06-21 21:18:45 -0400

    [FIX] invalid colors

--- src/palette/makeSplitComplementaryPalette.ts
+++ src/palette/makeSplitComplementaryPalette.ts
@@ -10,7 +10,7 @@ import { identifyFormat } from '../utils/identifyFormat'
  * @returns {Array<BaseColor>} An array of colors that make a color palette.
  * @throws {Error} If the parameter color does not follow its format requirements.
 */
-function makeSplitComplementaryPalette(color: BaseColor, quantity = 3): Array<BaseColor> {
+function makeSplitComplementaryPalette(color: BaseColor, variation = 50): Array<BaseColor> {
   const format = identifyFormat(color) as keyof ColorFormats
   const palette: Array<BaseColor> = [color]
   let h: number
@@ -18,8 +18,6 @@ function makeSplitComplementaryPalette(color: BaseColor, quantity = 3): Array<Ba
   let l: number
   let complementary1: BaseColor
   let complementary2: BaseColor
-  let complementarySplit1: BaseColor
-  let complementarySplit2: BaseColor
 
   // Get HSL value to manipulate Hue
   if (format === 'hsl') {
@@ -30,20 +28,27 @@ function makeSplitComplementaryPalette(color: BaseColor, quantity = 3): Array<Ba
     }).hsl as Hsl)
   }
 
-  // Calculate split-complementary colors
-  const hue1 = (h + 150) % 360
-  const hue2 = (h - 150) % 360
+  const complementaryHue = (h + 180) % 360
 
-  const complementaryHsl1: Hsl = { h: hue1, s, l}
-  const complementaryHsl2: Hsl = { h: hue2, s, l}
+  let complementaryHue1 = complementaryHue - variation
+  complementaryHue1 = complementaryHue1 < 0 ? complementaryHue1 + 360 : complementaryHue1
+
+  let complementaryHue2 = complementaryHue + variation
+  complementaryHue2 = complementaryHue2 > 360 ? complementaryHue2 - 360 : complementaryHue2
+
+  complementary1 = { h: complementaryHue1, s, l }
 
   if (format !== 'hsl') {
-    complementary1 = colorFormatConverter(complementaryHsl1, {
+    complementary1 = colorFormatConverter(complementary1, {
       currentFormat: 'hsl',
       targetFormat: [format]
     })[format] as BaseColor
+  }
 
-    complementary2 = colorFormatConverter(complementaryHsl2, {
+  complementary2 = { h: complementaryHue2, s, l }
+
+  if (format !== 'hsl') {
+    complementary2 = colorFormatConverter(complementary2, {
       currentFormat: 'hsl',
       targetFormat: [format]
     })[format] as BaseColor
@@ -51,33 +56,6 @@ function makeSplitComplementaryPalette(color: BaseColor, quantity = 3): Array<Ba
 
   palette.push(complementary1)
   palette.push(complementary2)
-
-  // Add complementary colors
-  if (quantity === 4) {
-    const hue3 = (h + 165) % 360
-    const complementarySplitHsl1: Hsl = { h: hue3, s, l}
-
-    if (format !== 'hsl') {
-      complementarySplit1 = colorFormatConverter(complementarySplitHsl1, {
-        currentFormat: 'hsl',
-        targetFormat: [format]
-      })[format] as BaseColor
-    }
-    palette.push(complementarySplit1)
-  }
-
-  if (quantity === 5) {
-    const hue4 = (h - 165) % 360
-    const complementarySplitHsl2: Hsl = { h: hue4, s, l}
-
-    if (format !== 'hsl') {
-      complementarySplit2 = colorFormatConverter(complementarySplitHsl2, {
-        currentFormat: 'hsl',
-        targetFormat: [format]
-      })[format] as BaseColor
-    }
-    palette.push(complementarySplit2)
-  }
 
   return palette
 }
